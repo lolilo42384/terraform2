ea4facb1386b1a8b932824e59f17bc704428130a
lolilo42384 <167253312+lolilo42384@users.noreply.github.com>
2025-04-01 01:12:05 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,51 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "pet-vpc"
-  }
+variable "vpc_id" {
+  default = "vpc-03a9fe9963aea016e"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+variable "subnet_id" {
+  default = "subnet-0fefe227dcd838fe9"
 }
 
 resource "aws_ecr_repository" "app_repo" {
@@ -69,4 +30,3 @@ resource "aws_ecr_repository" "app_repo" {
 resource "aws_ecs_cluster" "main" {
   name = "fastapi-cluster"
 }
-
